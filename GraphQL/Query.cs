04e188fc2dad6b95d74178a1536e262119b741b4
Morten la Cour <lacour@gmail.com>
2021-10-18 18:56:49 +0000

    Added resourceGroups query

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,7 +1,16 @@
+using bestilmere.common.azure;
+using bestilmere.common.azure.Models.Resources;
+using HotChocolate;
+
 namespace graphazure6
 {
     public class Query
     {
         public string GetVersion() => "0.3.0";
+
+        public Task<IEnumerable<ResourceGroupModel>?> GetResourceGroupsAsync(
+                    [Service] IResourceGroupHandler resourceGroupHandler,
+                    CancellationToken cancellationToken)
+                    => resourceGroupHandler.GetResourceGroupsAsync(cancellationToken: cancellationToken);
     }
 }
