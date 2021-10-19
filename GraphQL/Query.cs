366fe340791bd67fb88055cf0a49338e63da8d74
Morten la Cour <lacour@gmail.com>
2021-10-19 07:46:07 +0000

    Added GetRoleDefinitions Using 0.3.3

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -7,12 +7,18 @@ namespace graphazure6
 {
     public class Query
     {
-        public string GetVersion() => "0.3.1";
+        public string GetVersion() => "0.3.2";
 
         [UseFiltering]
         public Task<IEnumerable<ResourceGroupModel>?> GetResourceGroupsAsync(
                     [Service] IResourceGroupHandler resourceGroupHandler,
                     CancellationToken cancellationToken)
                     => resourceGroupHandler.GetResourceGroupsAsync(cancellationToken: cancellationToken);
+
+        [UseFiltering]
+        public Task<IEnumerable<RoleDefinitionModel>?> GetRoleDefitionsAsync(
+                [Service] IRoleHandler roleHandler,
+                CancellationToken cancellationToken)
+                => roleHandler.GetRoleDefinitionsAsync(cancellationToken: cancellationToken);
     }
 }
